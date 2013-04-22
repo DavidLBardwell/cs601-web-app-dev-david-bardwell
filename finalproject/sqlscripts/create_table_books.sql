84b8f6c9348a808b49acc311184f573ee8188006
DavidLBardwell <DavidLBardwell@gmail.com>
2013-04-22 03:13:41 +0000

    Adding new book images. Fixed bug with switching categories on main page. No longer use ajax as there are deep issues with jQuery/ajax and dataTables. Just post new category to session and then have the page request a reload.

--- finalproject/sqlscripts/create_table_books.sql
+++ finalproject/sqlscripts/create_table_books.sql
@@ -12,15 +12,16 @@ create table books (
 
 -- insert all the book data
 -- Fiction
-insert into books values (null, 'A Tale of Two Cities', 'Charles Dickens', 1859, 'Fiction', 5, 5.25, null);
+insert into books values (null, 'A Tale of Two Cities', 'Charles Dickens', 1859, 'Fiction', 5, 5.25, '../images/Tale_Two_Cities.jpg');
 insert into books values (null, 'Adventures of Huckleberry Finn', 'Mark Twain',  1884, 'Fiction', 3, 6.25, '../images/Huck_Finn.jpg');
-insert into books values (null, 'The Scarlett Letter', 'Nathaniel Hawthorne', 1850, 'Fiction', 4, 5.50, null);
+insert into books values (null, 'The Scarlett Letter', 'Nathaniel Hawthorne', 1850, 'Fiction', 4, 5.50, '../images/Scarlet_Letter.jpg');
 insert into books values (null, 'Great Expections', 'Charles Dickens', 1860, 'Fiction', 2, 8.50, '../images/Great_Expectations.jpg');
-insert into books values (null, 'Moby Dick', 'Herman Melville', 1851, 'Fiction', 3, 6.50, null);
+insert into books values (null, 'Moby Dick', 'Herman Melville', 1851, 'Fiction', 3, 6.50, '../images/Moby_Dick.jpg');
+insert into books values (null, 'The Great Gatsby', 'F. Scott Fitzgerald', 1928, 'Fiction', 4, 7.50, '../images/Great_Gatsby.jpg');
 
 -- Business
-insert into books values (null, 'The Art of The Deal', 'Donald Trump', 1987, 'Business', 4, 5.00, null);
-insert into books values (null, 'Creating Wealth', 'Robert G. Allen', 2006, 'Business', 2, 5.50, null);
-insert into books values (null, 'How To Make Money in Stocks', 'William Oneil', 2010, 'Business', 3, 6.75, null);
-insert into books values (null, 'Thinking Like a Billionaire', 'Donald Trump', 2004, 'Business', 2, 10.00, null);
+insert into books values (null, 'The Art of The Deal', 'Donald Trump', 1987, 'Business', 4, 5.00, '../images/Art_Of_The_Deal.jpg');
+insert into books values (null, 'Creating Wealth', 'Robert G. Allen', 2006, 'Business', 2, 5.50, '../images/Creating_Wealth.jpg');
+insert into books values (null, 'How To Make Money in Stocks', 'William Oneil', 2010, 'Business', 3, 6.75, '../images/Make_Money_In_Stocks.jpg');
+insert into books values (null, 'Thinking Like a Billionaire', 'Donald Trump', 2004, 'Business', 2, 10.00, '../images/Think_Like_A_Billionaire.jpg');
 
