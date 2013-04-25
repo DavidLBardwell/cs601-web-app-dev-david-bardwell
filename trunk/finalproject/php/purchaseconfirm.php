3e8f123c2274cc0a7d75f9ad0dc751ff1a34feff
DavidLBardwell@gmail.com <DavidLBardwell@gmail.com@81537c08-a02f-4d13-775b-79c2801132c2>
2013-04-25 23:41:22 +0000

    Added tooltip for password field. Worked on purchase confirmation screen with css selectors.

--- trunk/finalproject/php/purchaseconfirm.php
+++ trunk/finalproject/php/purchaseconfirm.php
@@ -1,25 +1,80 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <title>David's Second-hand Bookstore - Purchase Confirmation Page</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
+        <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.9.1.js">
+        </script>
         
+        <script type="text/javascript">
+            // return to the main bookstore page
+            $(document).ready(function() {
+                $("#returnToMainPage").on("click", function() {
+                    $.post(
+                        "index.php",
+                        {action : 'redisplay_bookstore_page'},
+                        function(result) {
+                            window.location = 'index.php?action=redisplay_bookstore_page';
+                        }
+                    );
+                });
+            });
+        
+        </script>
+        
+        <style type="text/css">
+            h1 {
+                color : green;
+            }
+            #mailto {
+                margin-top: 30px;
+            }
+            
+            #header1 {
+                margin-top: 20px;
+            }
+            
+            #return {
+                margin-top : 20px;
+            }
+            
+            /* sibling selector,direct descendant and pseudo selector to get successful message line
+               note: without first-child this would match two paragraph dom elements */
+            h1 + div > p:first-child {
+                color : red;
+                font-weight : bold;
+            }
+            
+            /* direct descendant to another direct descendant to get delivery text */
+            div > p > span {
+                color : orange;
+                font-weight : bold;
+                font-size: 150%;
+            }
+            
+            /* direct descendant to a class selector to get name */
+            div > .name {
+                color : purple;
+                font-weight : bold;
+                font-family : sans-serif;
+            }
+            
+            /* direct descendant and sibling to get all address lines not including name */
+            div > p + p {
+                color : green;
+                font-weight: bold;
+            }
+            
+
+        </style>
     </head>
     
     <body>
-        <h2>Purchase confirmation</h2>
+        <h1>Purchase confirmation</h1>
         
-        <div>
-            <p>Your purchase has been completed successfully. The books will be mailed to:</p>
+        <div id="header1">
+            <p id="successful">Your purchase has been completed successfully.</p>
+            <p id="mailBooksTo">The books will be mailed to:</p>
 <?php
     // retrive customer information 
     $customer = $_SESSION['customer'];
@@ -30,13 +85,24 @@
     $city = $customer->getCity();
     $state = $customer->getState();
     $zipcode = $customer->getZipcode();
-
-            echo "<p>" . $firstName . " " . $lastName . "</p>";
-            echo "<p>" . $address1 . "</p>";
-            echo "<p>" . $address2 . "</p>";
-            echo "<p>" . $city . ", " . $state . "  " . $zipcode . "</p>";
+    
+            echo "<div id='mailto'>";
+            echo "<p class='name'>" . $firstName . " " . $lastName . "</p>";
+            echo "<p class='address'>" . $address1 . "</p>";
+            echo "<p class='address'>" . $address2 . "</p>";
+            echo "<p class='address'>" . $city . ", " . $state . "  " . $zipcode . "</p>";
+            echo "</div>";
+            
+            echo "<div id='delivery'>";
+            echo "<p><span class='delivery'>The books will be delivered by " . $summary['delivery_method'] . " service.</span/</p>";
+            echo "</div>";
 ?>
+            
+            <div id="return">
+                <button id="returnToMainPage">Return to Main Page</button>
+                
+            </div>
             
         </div>
     </body>
-</html>    
+</html>
