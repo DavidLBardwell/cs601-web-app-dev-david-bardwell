e5cfba22eee2007be446eeaf84d13c293dbac5b6
DavidLBardwell <DavidLBardwell@gmail.com>
2014-10-16 21:06:45 +0000

    homework 2 - initial work on drag and drop.

--- homework/homework2/partyWise.js
+++ homework/homework2/partyWise.js
@@ -1,9 +1,11 @@
 
 var senatorList = [];
+var debugMsg;
 window.onload = function() {
   // see if our senators exist in localStorage. If so, load
   // up our JSON senator list from localStorage. If not, load
   // via an AJAX call from the partyList.xml file.
+  debugMsg = document.getElementById("debugMsg");
   if (window.localStorage.length > 0) {
       for (var i = 0; i < window.localStorage.length; i++) {
           var keyValue = localStorage.key(i);
@@ -73,7 +75,59 @@ function displaySenators() {
      membersHTML.innerHTML='';
      for (i = 0; i < senatorList.length; i++) {
          var nextLi = document.createElement("li");
+         nextLi.setAttribute("id", senatorList[i].name);
+         nextLi.setAttribute("draggable", true);
          nextLi.innerHTML = senatorList[i].name;
          membersHTML.appendChild(nextLi);
      }
+     
+     // Add event handlers for the source
+     membersHTML.ondragstart = dragStartHandler;
+     membersHTML.ondragend = dragEndHandler;
+     membersHTML.ondrag = dragHandler;     
+     
+     var republicanDrop = document.getElementById("republicans");
+     var democratDrop = document.getElementById("democrats");
+     
+    // Add event handlers for the target
+    republicanDrop.ondragenter = dragEnterHandler;
+    republicanDrop.ondragover = dragOverHandler;
+    republicanDrop.ondrop = dropHandler;
+
+    democratDrop.ondragenter = dragEnterHandler;
+    democratDrop.ondragover = dragOverHandler;
+    democratDrop.ondrop = dropHandler;
+}
+
+function dragStartHandler(e) {
+    e.dataTransfer.setData("Text", e.target.id);
+    debugMsg.innerHTML = "start dragging";
+}
+
+function dragEndHandler(e) {
+    debugMsg.innerHTML = "Drop over the target";
+}
+
+function dragHandler(e) {
+    debugMsg.innerHTML = "Dragging " + e.target.id;
+}
+
+function dragEnterHandler(e) {
+    debugMsg.innerHTML = "Drag Entering " + e.target.id;
+    e.preventDefault();
+}
+
+function dragOverHandler(e) {
+    debugMsg.innerHTML = "Drag Over " + e.target.id;
+    e.preventDefault();
+}
+
+function dropHandler(e) {
+//    var sourceId = e.dataTransfer.getData("Text");  
+//    var sourceElement = document.getElementById(sourceId);
+//    var newElement = sourceElement.cloneNode(false);               
+//    target.innerHTML = "";
+//    target.appendChild(newElement);
+    debugMsg.innerHTML = "Drop completed for " + e.target.id;
+    e.preventDefault();
 }
