21e85b1c27f57e62ad55ae85cf28d8c5af7a63a1
DavidLBardwell <DavidLBardwell@gmail.com>
2014-11-12 23:04:30 +0000

    Final project continue initial development - add items.xml for all Google search items.xml - links for initial search results - add autocomplete for entering a search.

--- cs701finalproject/js/initialMap.js
+++ cs701finalproject/js/initialMap.js
@@ -16,6 +16,16 @@ var mapData = [];
 var mapDataDetailObject = {};
 var mapDataDetail = [];
 
+var itemList = [];
+
+
+/**
+ * function showMap
+ * 
+ * This is called to show the initial search Google map. This function
+ * is re-entrant, and will create a new map object each time.
+ * 
+ */
 function showMap() {
     // asynchronous call with callback function specified
     var options = {
@@ -24,6 +34,8 @@ function showMap() {
       maximumAge : 0
     };
     
+    // Default to current location for the search. Other ways of establishing
+    // the starting latitude and longitude will be provided.
     navigator.geolocation.getCurrentPosition(
         displayInitialLocation, handleError, options);    
 }
@@ -89,48 +101,78 @@ function initializePlaces(pos) {
       zoom: 16
     });
 
-  var request = {
-    location: latLong,
-    radius: '1000',
-    types: ['store']
-  };
+    var request = {
+        location: latLong,
+        radius: $("#searchRadius").val(),
+        types: [$("#searchSelection").val()]
+    };
 
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callbackPlaces);
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callbackPlaces);
 }
 
 function callbackPlaces(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      //console.log(place);
-      var nextLatLong = place.geometry.location;
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        // reset mapData and search results for next search
+        mapData = [];
+        $("#searchResultsTable").empty();
+        
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            //console.log(place);
+            var nextLatLong = place.geometry.location;
       
-      // add the marker to the map
-      var title = place.name;
-      var content = "Lat: " + nextLatLong.B + 
-                    ", Long: " + nextLatLong.k;
+            // add the marker to the map
+            var title = place.name;
+            var iconUrl = place.icon;
+            var content = "Lat: " + nextLatLong.B + 
+                        ", Long: " + nextLatLong.k;
             
-      addMarker(map, nextLatLong, title, content);
+            addMarker(map, nextLatLong, title, content, iconUrl);
       
-      $("#searchResultsTable").append("<tr><td>" + title +  "</td></tr>");
-      
-      // save important places information 
-      var nextPlaceID = place.place_id;
-      var jSONPlace = {title : title, place_id : nextPlaceID};
-      mapData.push(jSONPlace);
+            $("#searchResultsTable").append("<tr><td><a id='locationLink" + i + "' class='dynamic-link' href='#'>" + title + "</a></td></tr>");
+            
+            // save important places information 
+            var nextPlaceID = place.place_id;
+            var jSONPlace = {title : title, place_id : nextPlaceID, icon : iconUrl};
+            mapData.push(jSONPlace);
+        }
+        
+        $('#searchResultsTableHeader').html('Search Result');
+        if (results.length > 0) {
+            $('#searchResultsTableHeader').show();
+        }
+        else {
+            $('#searchResultsTableHeader').hide();
+        }
+        
+        // need to dynamically bind the anchors, and this should work well
+        $('a.dynamic-link').click(function() {
+            var linkId = this.id;
+            var offset = linkId.substr(12);
+            getDetailPlaces(offset);
+            return false;
+        });
+        
+        mapDataObject.mapData = mapData;
     }
-    mapDataObject.mapData = mapData;
-  }
+    else if (status === "ZERO_RESULTS") {
+        // reset mapData and search results for next search
+        mapData = [];
+        $('#searchResultsTableHeader').html('No Results Found');
+        $('#searchResultsTableHeader').show();
+        $("#searchResultsTable").empty();   
+    }
 }
 
 // add position marker to the map
-function addMarker(map, latlongPosition, title, content) {
+function addMarker(map, latlongPosition, title, content, iconUrl) {
    
     var options = {
         position: latlongPosition,
         map: map,
         title: title,
+        icon : iconUrl,
         clickable: true
     };
     var marker = new google.maps.Marker(options);
@@ -147,10 +189,16 @@ function addMarker(map, latlongPosition, title, content) {
     });
 }
 
-function getDetailPlaces() {
+/**
+ * function getDetailPlaces()
+ * @argument location_offset - index into mapData to show detail
+ * This gets specific detail information about a specific place
+ * 
+ */
+function getDetailPlaces(location_offset) {
     // test - see if we can get the detail places call to work
     var request = {
-        placeId: mapDataObject.mapData[0].place_id
+        placeId: mapDataObject.mapData[location_offset].place_id
     };
 
     service = new google.maps.places.PlacesService(map);
@@ -161,10 +209,17 @@ function callbackDetail(place, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         
         mapDataDetailObject.name = place.name;
+        mapDataDetail = [];
         
-        // display all the photos 
-        var photoCount = place.photos.length;
-        var photos = place.photos;
+        // display all the photos, note: need to check if there are any
+        var photoCount = 0;
+        var photos = null;
+                
+        if (place.hasOwnProperty('photos') === true) {
+            photos = place.photos;
+            photoCount = photos.length;
+        }
+        
         for (var i = 0; i < photoCount; i++) {
             var urlPhoto = photos[i].getUrl({ 'maxWidth': 200, 'maxHeight': 200 });
             var jSONData = {url : urlPhoto};
@@ -186,6 +241,7 @@ function callbackDetail(place, status) {
         // display the details screen
         $("#locationTitle").html(mapDataDetailObject.name);
         
+        $("#photos").empty();
         for (var i = 0; i < mapDataDetail.length; i++) {
             $("#photos").append("<img src='" + mapDataDetail[i].url + "'>");
             if (((i + 1) % 4) === 0) {
@@ -196,5 +252,28 @@ function callbackDetail(place, status) {
         $("#target").tabs("select", 1 );
     }
 }    
-   
 
+function getGoogleSearchItems() {
+    // Read all possible Google Place search items from the items.xml file
+    // so this is not hard-coded.
+    itemList = [];  // clear, just in case
+    
+    $.ajax({
+        url : "items.xml",
+        type : "GET",
+        dataType: "xml",
+        cache: false,
+        success: function(result) {
+            var response = result;
+            // for each <item> element
+            $(response).find("item").each (function() {
+                var name = $(this).find("name").text();
+                var item = {label : name, 
+                            value : name,
+                            data : {name : name}};
+                itemList.push(item); 
+            });
+            $('#searchSelection').removeAttr("disabled");
+        }
+    });
+}
