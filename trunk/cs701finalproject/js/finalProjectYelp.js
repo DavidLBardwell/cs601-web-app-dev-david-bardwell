f24e80c4bc90f412c6e39ff766fbc95c9a13c175
DavidLBardwell@gmail.com <DavidLBardwell@gmail.com@81537c08-a02f-4d13-775b-79c2801132c2>
2014-12-01 02:54:39 +0000

    Add additional Yelp reviews for nearby locations to fill in the 3rd tab better.

--- trunk/cs701finalproject/js/finalProjectYelp.js
+++ trunk/cs701finalproject/js/finalProjectYelp.js
@@ -5,14 +5,22 @@
 // the specific authorization implementation needed to call the Yelp API.
 
 function yelpAPICallout(term, location, cll) {
-    /* var term = 'restaurant';
+    /* Yelp API takes parameters as follows:
+    var term = 'restaurant';
     var location = '10 Wayside Road, Burlington, MA 01803';
-    var cll = '42.4857853,-71.1911223'; */
+    var cll = '42.4857853,-71.1911223'; 
+    */
     yelpResults = [];
+    yelpSearchLocation = location;
     
     $("#YelpReviews").empty();
     $("#YelpReviews").append("<h3>Running Yelp API query, please wait...<br/>");
             
+    // The ajax call is to a server-side stub which in turn calls into the
+    // YelpAPI.php code to get up to 15 businesses (hopefully, one of which
+    // is the one we are asking for). The Yelp API has a significant precision
+    // issue and does not always return the specific item of interest. We will
+    // thus show the user locations near the one they requested.
     $.ajax({
         url: "php/yelpstub.php",
         data : {term : term, location : location, cll : cll },
@@ -52,14 +60,14 @@ function yelpAPICallout(term, location, cll) {
             if (matchIndex !== -1) {
                 if (yelpResults[matchIndex].reviews !== undefined) {
                     $("#YelpReviews").empty();
-                    $("#YelpReviews").append("<h3>Review ratings comments for " + yelpResults[matchIndex].name + "</h3><br/>");
+                    $("#YelpReviews").append("<h3>Review rating and comments for <span id='yelpLocation'>" + yelpResults[matchIndex].name + "</span></h3><br/>");
                     for (var i = 0; i < yelpResults[matchIndex].reviews.length; i++) {
                         var nextReview = yelpResults[matchIndex].reviews[i];
                         if (i === 0) {
                             $("#YelpReviews").append("<table><thead><tr><th>Rating</th><th>Comments</th></tr></thead>");
                             $("#YelpReviews").append("<tbody>");
                         }
-                        $("#YelpReviews").append("<tr><td>" + nextReview.rating + "</td>");
+                        $("#YelpReviews").append("<tr><td><span id='yelpRating'>" + nextReview.rating + "</span></td>");
                         $("#YelpReviews").append("<td>" + nextReview.excerpt + "</td></tr>");
                     }
                     $("#YelpReviews").append("</tbody></table><br/>");
@@ -70,8 +78,30 @@ function yelpAPICallout(term, location, cll) {
             }
             else {
                 $("#YelpReviews").empty();
-                $("#YelpReviews").append("Sorry, location match not successful for a Yelp review search.");
+                $("#YelpReviews").append("Sorry, location match not successful for <span id='yelpLocation'>" + mapDataDetailObject.name + "</span> in a Yelp review search.");
             }
+            
+            // show other results from near-by businesses since we went to all the trouble of 
+            // getting this very valuable information.
+            var displayTableHeader = false;
+            for (var i = 0; i < yelpResults.length; i++) {
+                if (i === matchIndex) {
+                    continue;
+                }
+                var nextReview = yelpResults[i].reviews[0];
+                if (displayTableHeader === false) {
+                    displayTableHeader = true;
+                    $("#YelpReviews").append("<p></p><h3>Review ratings and comments for additional nearby locations</h3><br/>");
+                    $("#YelpReviews").append("<table><thead><tr><th>Name</th><th>Rating</th><th>Comments</th></tr></thead>");
+                    $("#YelpReviews").append("<tbody>");
+                }
+                $("#YelpReviews").append("<tr><td><span id='yelpLocation'>" + yelpResults[i].name + "</span></td>");
+                $("#YelpReviews").append("<td><span id='yelpRating'>" + nextReview.rating + "</span></td>");
+                $("#YelpReviews").append("<td>" + nextReview.excerpt + "</td></tr>");
+            }
+            if (displayTableHeader === true) {
+                $("#YelpReviews").append("</tbody></table><br/>");
+            }    
         }
     });
 }
